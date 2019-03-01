42fd4ef5cb89ca38060c039fb4e3da5f52c42448
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-01 13:25:58 +0200

    Fix import Container
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Container from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 import './App.css';
 
 class App extends Component {
