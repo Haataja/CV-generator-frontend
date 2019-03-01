a61bfaf7309389d0633a536dbb29de0859fce9f9
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-01 14:20:43 +0200

    Ribbon: Add ButtonToolBar and Button
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- src/Ribbon.js
+++ src/Ribbon.js
@@ -10,9 +10,15 @@ class Ribbon extends Component {
     }
 
     render() {
-        return <div>
-            <h1>This is a test</h1>
+        return (
+
+        <div>
+            <ButtonToolbar/>
+                <Button variant="primary">Document</Button>
+            <ButtonToolbar/>
         </div>
+
+        );
     }
 }
 
