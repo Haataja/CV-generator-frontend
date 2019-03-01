3d9a5566c13b4954c8cdcc98bb2ceafd9f050700
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-01 23:11:18 +0200

    App: Add Document to render()
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ class App extends Component {
         </Row>
         <Row>
           <Col xs={12}>
-            <Document/>
+            <Document />
           </Col>
         </Row>
       </Container>
