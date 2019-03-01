78659125c06b12c8a8ff1426690078fce2aec00e
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-01 16:00:22 +0200

    Ribbon: Change Buttons to Nav
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- src/Ribbon.js
+++ src/Ribbon.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
-import Button from "react-bootstrap/Button";
-import ButtonToolbar from "react-bootstrap/ButtonToolbar";
+
 
 import './Ribbon.css'
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Nav from "react-bootstrap/Nav";
 
 class Ribbon extends Component {
   constructor(props) {
     super(props);
+    this.handleSelect = this.handleSelect.bind(this);
+
+  }
+
+  handleSelect(eventKey) {
+    console.log(eventKey);
   }
 
   render() {
     return (
-      <div>
-        <ButtonToolbar/>
-        <Button variant="primary" className="menu_button">Document</Button>
-        <ButtonToolbar/>
+      <div id="ribbon">
+        <Nav variant='pills' onSelect={this.handleSelect}>
+          <NavDropdown title='Document'>
+            <NavDropdown.Item eventKey='1'>Edit CV</NavDropdown.Item>
+            <NavDropdown.Item eventKey='2'>Print CV</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Item>
+            <Nav.Link eventKey='3'>Timeline</Nav.Link>
+          </Nav.Item>
+        </Nav>
       </div>
-
     );
   }
 }
