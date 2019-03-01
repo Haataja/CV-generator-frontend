a4a434aed6996e22d4aa6f80ed76352ac26653b1
Jindetta <joonas.lauhala@cs.tamk.fi>
2019-03-01 14:03:50 +0200

    Document: Define base functions

--- src/Document.js
+++ src/Document.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
 class Document extends Component {
+  constructor(props) {
+    super(props);
 
+    this.data = props.data;
+  }
+  render() {
+    return "testi";
+  }
 }
 
 export default Document;
