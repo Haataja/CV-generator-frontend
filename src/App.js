2e0aae08459943addc9cfbe4f3385db5cb40d427
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-01 13:34:28 +0200

    Setup App layout
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+
 import Container from 'react-bootstrap/Container';
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Ribbon from "./Ribbon";
+
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
         <Container fluid={true}>
-
+            <Row>
+                <Col xs={12}>
+                    <Ribbon/>
+                </Col>
+            </Row>
         </Container>
     );
   }
