003322df8ca359267a60761fd8feedd41f5d1eb8
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-01 23:19:38 +0200

    DocumentControls: Render dummy data
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- src/components/DocumentControls.js
+++ src/components/DocumentControls.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 
+import { Container, Row, Col } from 'react-bootstrap';
+
 export default class DocumentControls extends Component {
   render() {
-    return null;
+    return (
+      <Container fluid={true} id="controls">
+        <Row>
+          <Col xs={12}>
+            Setting value
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
