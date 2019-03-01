1fa7c6a0ae492a61e54c08c86315442b21f079f7
Jindetta <joonas.lauhala@cs.tamk.fi>
2019-03-01 14:04:07 +0200

    DocumentControls: Define base functions

--- src/DocumentControls.js
+++ src/DocumentControls.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
 class DocumentControls extends Component {
+  constructor(props) {
+    super(props);
 
+    this.document = props.document;
+  }
+  render() {
+    return "testi";
+  }
 }
 
 export default DocumentControls;
