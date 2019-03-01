7e1e7f5320886ddaf85a01d05fd629b171c1df08
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-01 14:22:33 +0200

    Add Ribbon.css
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- src/Ribbon.js
+++ src/Ribbon.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Button from "react-bootstrap/Button";
 import ButtonToolbar from "react-bootstrap/ButtonToolbar";
 
+import './Ribbon.css'
+
 class Ribbon extends Component {
     constructor(props) {
         super(props);
