583a0cbd4e8d0bc2a434feb104069039c55b6028
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-01 18:23:46 +0200

    DocumentControls: Delete constructor()
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- src/DocumentControls.js
+++ src/DocumentControls.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 
 class DocumentControls extends Component {
-  constructor(props) {
-    super(props);
-
-    this.document = props.document;
-  }
   render() {
     return (
       <div id="controls">
