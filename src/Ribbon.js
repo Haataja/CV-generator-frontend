a1d181dcb240f8d0f5145dfc31a84cfcbbbdea43
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-01 14:18:47 +0200

    Ribbon: Add constructor()
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- src/Ribbon.js
+++ src/Ribbon.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import Button from "react-bootstrap/Button";
+import ButtonToolbar from "react-bootstrap/ButtonToolbar";
 
 class Ribbon extends Component {
+    constructor(props) {
+        super(props);
+
+        this.data = this.props.data;
+    }
+
     render() {
         return <div>
             <h1>This is a test</h1>
