ee2abaf7b7d68bd6fd521e87e1f20026e1c53402
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-01 23:12:58 +0200

    Document: Render dummy data
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- src/components/Document.js
+++ src/components/Document.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { Container, Row, Col } from 'react-bootstrap';
+
+import DocumentControls from "./DocumentControls";
+
 export class Document extends Component {
   render() {
-    return null;
+    return (
+      <Container fluid={true} id="document">
+        <Row>
+          <Col xs={9}>
+            <Container id="page">
+              <div id="header">
+                <Row>
+                  <Col xs={6}>
+                    Nothing
+                  </Col>
+                  <Col xs={4}>
+                    Resume
+                  </Col>
+                  <Col xs={2}>
+                    Page
+                  </Col>
+                </Row>
+              </div>
+              <div id="content">
+                <Row>
+                  <Col xs={12}>
+                    Content goes here...
+                  </Col>
+                </Row>
+              </div>
+              <div id="footer">
+                <Row className="text-center">
+                  <Col xs={12}>
+                    Footer content
+                  </Col>
+                </Row>
+              </div>
+            </Container>
+          </Col>
+          <Col xs={3}>
+            <DocumentControls />
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
 
