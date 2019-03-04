2a6978b93742691ad1c37b57a9b3af58e3c0aa8f
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-04 17:23:08 +0200

    Ribbon: Change className for flag -images.
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- src/components/Ribbon.js
+++ src/components/Ribbon.js
@@ -61,14 +61,14 @@ export class Ribbon extends Component {
 
               <Nav.Item>
                 <Nav.Link eventKey='4'>
-                  <img className="Flag" src={british_flag} width={50} height={30} alt="Logo English" />
+                  <img className="flag" src={british_flag} width={50} height={30} alt="Logo English" />
                   English
                 </Nav.Link>
               </Nav.Item>
 
               <Nav.Item>
                 <Nav.Link eventKey='5'>
-                  <img className="Flag"  src={finnish_flag} width={50} height={30} alt="Logo Finnish" />
+                  <img className="flag"  src={finnish_flag} width={50} height={30} alt="Logo Finnish" />
                   Finnish
                 </Nav.Link>
               </Nav.Item>
