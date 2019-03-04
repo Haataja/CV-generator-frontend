6434e2bcd435552de2f9ba3d1ff04fd31ba07f2e
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-04 11:06:49 +0200

    Ribbon: add changes that were left out of merge from dev
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- src/components/Ribbon.js
+++ src/components/Ribbon.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 export class Ribbon extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSelect = this.handleSelect.bind(this);
+
+  }
+
+  handleSelect(eventKey) {
+    console.log(eventKey);
+  }
+
   render() {
-    return null;
+    return (
+      <div id="ribbon">
+        <Nav variant='pills' onSelect={this.handleSelect}>
+          <NavDropdown title='Document'>
+            <NavDropdown.Item eventKey='1'>Edit CV</NavDropdown.Item>
+            <NavDropdown.Item eventKey='2'>Print CV</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Item>
+            <Nav.Link eventKey='3'>Timeline</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </div>
+    );
   }
 }
 
