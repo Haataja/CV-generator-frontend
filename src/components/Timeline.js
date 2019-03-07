e109a6b829c8716a091dc80e97048477061b9b1e
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-07 21:59:55 +0200

    Timeline: Add Buttons for showing history
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {Col, Container, Row} from "react-bootstrap";
 import './Timeline.css';
+import Button from "react-bootstrap/Button";
 
 
 export class Timeline extends Component {
@@ -9,7 +10,15 @@ export class Timeline extends Component {
     return (
       <div id="background">
         <Container id="page">
-
+          <Col>
+            <Button>Show work</Button>
+          </Col>
+          <Col className="button-col">
+            <Button>Show education</Button>
+          </Col>
+          <Col className="button-col">
+            <Button>Show projects</Button>
+          </Col>
         </Container>
       </div>
     );
