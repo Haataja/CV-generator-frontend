3604fcb9c952ccd7bcddef04490386d841da27c0
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-07 22:31:58 +0200

    Timeline: Add buttonClick()
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -3,20 +3,36 @@ import { connect } from 'react-redux';
 import {Col, Container, Row} from "react-bootstrap";
 import './Timeline.css';
 import Button from "react-bootstrap/Button";
+import warning from "react-redux/es/utils/warning";
 
 
 export class Timeline extends Component {
+  constructor(props) {
+    super(props);
+
+    this.buttonClick.bind(this);
+  }
+  buttonClick(event) {
+    let textContent = event.target.textContent;
+
+    if(textContent.search("Show") === 0) {
+      event.target.textContent = event.target.textContent.replace('Show', 'Hide');
+    } else {
+      event.target.textContent = event.target.textContent = event.target.textContent.replace('Hide','Show');
+    }
+  }
+
   render() {
     return (
       <div id="background">
         <Container id="page">
           <Col>
-            <Button>Show work</Button>
+            <Button onClick={this.buttonClick}>Show work</Button>
           </Col>
-          <Col className="button-col">
+          <Col onClick={this.buttonClick} className="button-col">
             <Button>Show education</Button>
           </Col>
-          <Col className="button-col">
+          <Col onClick={this.buttonClick} className="button-col">
             <Button>Show projects</Button>
           </Col>
         </Container>
