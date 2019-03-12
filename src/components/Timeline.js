49b5321f9989129a20078f2452ef584d75eb7f08
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-12 15:55:43 +0200

    Timeline: Add import for TimelineActions
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+import * as actions from "../actions/TimelineActions";
+
 class Timeline extends Component {
   render() {
     return null;
