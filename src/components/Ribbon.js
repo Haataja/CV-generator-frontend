d66f2d3782733b07e15414af3359a6a14ef20a7d
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-12 15:56:22 +0200

    Ribbon: Add import for RibbonActions
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- src/components/Ribbon.js
+++ src/components/Ribbon.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+import * as actions from "../actions/RibbonActions";
+
 class Ribbon extends Component {
   render() {
     return null;
