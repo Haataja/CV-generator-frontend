db53fca5d3c11ec7d35b0169cdb27fdcd5570a13
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-12 15:52:21 +0200

    Timeline: Update class export definition
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export class Timeline extends Component {
+class Timeline extends Component {
   render() {
     return null;
   }
