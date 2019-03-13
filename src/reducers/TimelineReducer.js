e0432b65561be6b340eb7e7be2b13fbd100c3a5e
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-13 17:07:31 +0200

    TimelineReducers: Add SHOW_PROJECTS and SHOW_EDUCATION to switch
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- src/reducers/TimelineReducer.js
+++ src/reducers/TimelineReducer.js
@@ -4,8 +4,16 @@ const DEFAULT_STATE = {
 
 export default function reducer(state = DEFAULT_STATE, action) {
   switch (action.type) {
-    case 'SHOW_WORK':
+    case 'SHOW_WORK': {
       state = {...state, SHOW_WORK: action.payload};
+      break;
+    }
+    case 'SHOW_PROJECTS': {
+      state = {...state, SHOW_PROJECTS: action.payload};
+      break;
+    }
+    case 'SHOW_EDUCATION':
+      state = {...state, SHOW_EDUCATION: action.payload};
       break;
   }
   return state;
