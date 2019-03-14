25e5de6840bb9b8ad7ce8ac74eda5beb18109367
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-14 02:48:35 +0200

    App: Use DocumentView in render()
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Container, Col, Row} from 'react-bootstrap';
 
 import Ribbon from './Ribbon';
+import DocumentView from "./DocumentView";
 
 import './App.css';
 
@@ -16,7 +17,7 @@ class App extends Component {
         </Row>
         <Row>
           <Col xs={12}>
-
+            <DocumentView/>
           </Col>
         </Row>
       </Container>
