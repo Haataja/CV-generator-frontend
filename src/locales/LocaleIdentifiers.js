8234ff83879c7e59edfcf55141243964eab7a0a9
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-14 02:06:36 +0200

    Initial commit
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

---
+++ src/locales/LocaleIdentifiers.js
@@ -0,0 +1,14 @@
+export default {
+  GLOBAL_ID: "global_language_id",
+  GLOBAL_LANGUAGE: "global_language",
+
+  RIBBON_EDITOR: "ribbon_item_editor",
+  RIBBON_TIMELINE: "ribbon_item_timeline",
+
+  TIMELINE_SHOW_WORK: "timeline_setting_show_work",
+  TIMELINE_HIDE_WORK: "timeline_setting_hide_work",
+  TIMELINE_SHOW_EDUCATION: "timeline_setting_show_education",
+  TIMELINE_HIDE_EDUCATION: "timeline_setting_hide_education",
+  TIMELINE_SHOW_PROJECTS: "timeline_setting_show_projects",
+  TIMELINE_HIDE_PROJECTS: "timeline_setting_hide_projects"
+};
