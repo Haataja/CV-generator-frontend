e084bb2c9b84b5f8b8de1b8b0e409c8d0fe38e1e
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-19 09:57:09 +0200

    App: Merged Ribbon and Timeline

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Container, Col, Row} from 'react-bootstrap';
 
 import Timeline from "./TimelineView";
+import Ribbon from "./Ribbon";
 
 import './App.css';
 
@@ -9,6 +10,11 @@ class App extends Component {
   render() {
     return (
       <Container fluid={true}>
+          <Row>
+              <Col xs={12}>
+                  <Ribbon/>
+              </Col>
+          </Row>
         <Row>
           <Col xs={12}>
             <Timeline/>
