8ab168a5c77e60069f7e9a579c1e709afdd4abc6
Jindetta <joonas.lauhala@cs.tamk.fi>
2019-03-19 10:35:45 +0200

    RibbonReducer: Reset reducer state

--- src/reducers/RibbonReducer.js
+++ src/reducers/RibbonReducer.js
@@ -1,12 +1,7 @@
 const DEFAULT_STATE = {
-  LANGUAGE: 'en'
+
 };
 
 export default (state = DEFAULT_STATE, action) => {
-  switch (action.type) {
-    case 'LANGUAGE': {
-      return {...state, LANGUAGE: action.payload};
-    }
-  }
-  return state;
+  return {...state, [action.type]: action.payload};
 }
