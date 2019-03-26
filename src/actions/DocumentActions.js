47df3e235996fbfc69adf516bf7b4156a558d84c
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-26 23:25:40 +0200

    DocumentActions: Add setDialogEditMode() action
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- src/actions/DocumentActions.js
+++ src/actions/DocumentActions.js
@@ -1,9 +1,16 @@
 export const EDITOR_DIALOG = 'EDITOR_DIALOG';
-export const EDITOR_SIDEBAR = 'EDITOR_SIDEBAR';
+export const EDITOR_DIALOG_MODE = 'EDITOR_DIALOG_MODE';
 
 export function updateDialog(type) {
   return {
     type: EDITOR_DIALOG,
     payload: type
   };
+}
+
+export function setDialogEditMode(mode) {
+  return {
+    type: EDITOR_DIALOG_MODE,
+    payload: mode
+  }
 }
